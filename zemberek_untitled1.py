b94867f8fce99722d12b814bdee1157fa4f4173d
ugur can yaman <37077627+ucytv@users.noreply.github.com>
2021-10-22 03:13:02 +0300

    Add files via upload

---
+++ zemberek_untitled1.py
@@ -0,0 +1,15 @@
+import time
+import logging
+
+from zemberek import (
+    TurkishSpellChecker,
+    TurkishSentenceNormalizer,
+    TurkishSentenceExtractor,
+    TurkishMorphology,
+    TurkishTokenizer
+)
+
+morphology = TurkishMorphology.builder().build()
+ 
+
+
